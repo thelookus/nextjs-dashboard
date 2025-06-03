5d89b8bce1da4b847eff978446ff2652bc6ac918
thelookus <calvino.lucas@gmail.com>
2025-06-03 11:51:07 -0300

    Add Invoices functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,17 @@
+import { geist } from './ui/fonts';
+import './ui/global.css';
+import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Acme Dashboard',
+    template: '%s | Acme Dashboard'
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${geist.className} antialiased`}>
+        {children}
+        <footer className="py-2 flex justify-center items-center">
+          <p>Copyright © {new Date().getFullYear()} | Made with ❤️ by <Link className="hover:underline" href="https://www.linkedin.com/in/lucascalvino">Lucas Calviño</Link></p>
+        </footer>
+      </body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Geist, Geist_Mono, Lusitana } from 'next/font/google'
+
+export const geist = Geist({ subsets: ['latin'] })
+export const geistMono = Geist_Mono({ subsets: ['latin'] })
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
